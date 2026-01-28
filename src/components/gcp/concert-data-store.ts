e4e9caae608398963d2f903b76ab26968e5712fe
pannpers <pepperoni9@gmail.com>
2026-01-28 17:05:27 +0900

    fix: ignore changes to advancedSiteSearchConfig
    Prevents unintended resource replacement due to diffs in advancedSiteSearchConfig.

--- src/components/gcp/concert-data-store.ts
+++ src/components/gcp/concert-data-store.ts
@@ -33,14 +33,10 @@ export class ConcertDataStore extends pulumi.ComponentResource {
         contentConfig: 'PUBLIC_WEBSITE',
         solutionTypes: ['SOLUTION_TYPE_SEARCH'],
         createAdvancedSiteSearch: true, // Required for Extractive Segments
-        advancedSiteSearchConfig: {
-          disableAutomaticRefresh: false,
-          disableInitialIndex: false,
-        },
         skipDefaultSchemaCreation: false,
         project: projectId,
       },
-      { dependsOn: enabledServices }
+      { dependsOn: enabledServices, ignoreChanges: ['advancedSiteSearchConfig'] }
     )
 
     // Set the target site for the data store to crawl.
